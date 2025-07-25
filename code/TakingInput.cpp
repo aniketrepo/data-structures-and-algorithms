4a5c790b5284e850fabe6b262bde0da664ff094b
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-07-25 21:30:13 +0530

    Create TakingInput.cpp

---
+++ code/TakingInput.cpp
@@ -0,0 +1,12 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x, y;
+
+    cin >> "Enter the value of x: " >> x >> " and y: " >> y;
+    cout << "The value of x is " << x << " and y is " << y;
+
+    return 0;
+}
