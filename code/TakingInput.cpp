4c77fc43e263d3f7acea79052be7aa0f90d90f54
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-08-20 17:55:16 +0530

    Update TakingInput.cpp

--- code/TakingInput.cpp
+++ code/TakingInput.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main(){
 
     int x;
+    int y;
 
-    cout << "Enter a number: ";
-    cin >> x;
-    cout << "Value of x: " << x << endl;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+    cout << "Value of x: " << x << ", and value of y: " << y << endl;
 
     return 0;
 }
