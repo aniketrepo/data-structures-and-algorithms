0e5406f3fb24034148d5535db4a468ace287dc27
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-08-20 17:52:45 +0530

    Create TakingInput.cpp

---
+++ code/TakingInput.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Value of x: " << x << endl;
+
+    return 0;
+}
