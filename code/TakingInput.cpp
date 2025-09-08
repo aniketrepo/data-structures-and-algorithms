d4308f39f49a6f85d6221c1d33e7ff178b79cdd2
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-09-08 18:52:22 +0530

    Add program to take and display two integer inputs
    Introduces TakingInput.cpp, which reads two integers from standard input and prints their values. Useful for basic input/output demonstration.

---
+++ code/TakingInput.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int x,y;
+    
+    cin >> x >> y;
+    cout << "Value of x: " << x << "\n";
+    cout << "Value of y: " << y << "\n";
+
+    return 0;
+}
