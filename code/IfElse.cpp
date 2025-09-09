7d856eeeb1e7dd0bfa271dba26a7c23805fdc295
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-09-09 18:23:53 +0530

    Refactor if-else formatting in IfElse.cpp
    Updated the formatting of if-else statements to place 'else' and 'else if' on new lines with opening braces on separate lines for improved readability and consistency.

--- code/IfElse.cpp
+++ code/IfElse.cpp
@@ -10,9 +10,12 @@ int main() {
     
     if (age <= 18) {
         cout << "Less than 18, may not enter." << endl;
-    } else if (age == 18) {
+    }
+    else if (age == 18)
+    {
         cout << "Equal to 18, may enter." << endl;
-    } else {
+    }
+    else {
         cout << "Greater than 18, may enter." << endl;
     }
 
