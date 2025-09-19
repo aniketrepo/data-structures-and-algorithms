9ab746c12dc2b486ca154d793484b8b935a62b22
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2025-09-19 20:24:42 +0530

    Create WhileLoop.cpp

---
+++ code/WhileLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 2;
+
+    do {
+        cout << "Aniket " << i << endl;
+        i++;
+    } while (i <= 1);
+
+    cout << i;
+    
+    return 0;
+}
